a17cdcbbb67b47e42522994f070d9ef33deacf97
Mathew Zaleski <matz@zaleski.ca>
2018-06-11 16:12:03 -0400

    nasty javascript gotcha concerning this explained/fixed

--- src/index.js
+++ src/index.js
@@ -2,36 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/SearchBar';
 import GifList from './components/GifList';
+import request from 'superagent';
+
 
 class App extends React.Component {
     constructor() {
         super();
 
         this.state = {
-            gifs: [
-                {
-                    id: 1,
-                    url: 'http://fakeimg.pl/300/'
-                },
-                {
-                    id: 2,
-                    url: 'http://fakeimg.pl/300/'
-                },
-                {
-                    id: 3,
-                    url: 'http://fakeimg.pl/300/'
-                }
-            ]
+            gifs: []
         }
     }
     handleTermChange(term) {
-        console.log(term);
+        //concoct a giphy search URL from term
+        const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=dc6zaTOxFJmzC`;
+        request.get(url, (err, res) => {
+            this.setState({gifs: res.body.data})
+            console.log(term);
+        });
     }
-    render() {
+    render(){
         //stuff onTermChange property into searchbar equal to handleTermChange function above
+        //<SearchBar onTermChange={this.handleTermChange}/>
+        //if define onTermChange as a fat arrow function instead of just ref to function this will refer to App.this
         return (
             <div>
-                <SearchBar onTermChange={this.handleTermChange}/>
+                <SearchBar onTermChange={term => this.handleTermChange(term)}/>
                 <GifList gifs={this.state.gifs} />
 
             </div>
