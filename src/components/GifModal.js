bf912bc8dde7f41d7ec5b24512c753f3c8981e44
Mathew Zaleski <matz@zaleski.ca>
2018-06-13 16:14:12 -0400

    use prop-types to check that required props have been set. (thx joey)

--- src/components/GifModal.js
+++ src/components/GifModal.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Modal from 'react-modal';
+import PropTypes from 'prop-types'; // for checking types of props
 
 //connect up the props in react-model with the ones we made in app. (this feels bogus)
 const GifModal = (props) => {
-    if (!props.selectedGif){
-        console.log("GifModal: rendering to empty div because no selectedGif")
+    if (!props.modalIsOpen){
         return <div></div>
     }
-    console.log('rendering GifModal');
+    console.assert(props.selectedGif !=null) //never modalIsOpen and selectedGif not set to gif!
     return (
         <Modal
             isOpen={ props.modalIsOpen }
@@ -22,4 +22,24 @@ const GifModal = (props) => {
     );
 };
 
+//investigate rudeness of debugging in presence of typos setting prop names
+GifModal.propTypes = {
+    modalIsOpen: PropTypes.bool.isRequired,//PropTypes.bool.isRequired,
+    onRequestClose: PropTypes.func.isRequired,
+    //challenge is that initializing selectedGif to null will cause any.isRequired to fail
+    //whereas below will pass.
+    selectedGif: function(props, propName, componentName) {
+        if (! (propName in props)){
+            return new Error('selectedGif prop must exist and be set (to null or a Gif). Validation failed.' );
+        }
+        var prop = props[propName]
+        if (prop == null ){
+            return; //null is okay when there is no modal
+        }
+        if (! prop.hasOwnProperty('source')){
+            return new Error(propName + ' prop must have field "source". You sure you passed a Gif?' +
+                ' Validation failed for:' + prop.toString());
+        }
+    }
+}
 export default GifModal;
